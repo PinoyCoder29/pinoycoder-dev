c0d78c2b8ded4d36c9c4bb9f755fbd9681a16fb5
PinoyCoder29 <bicojayvee4@gmail.com>
2026-06-08 23:27:31 +0800

    adding design in projects

--- client/components/sections/ProjectSection/page.tsx
+++ client/components/sections/ProjectSection/page.tsx
@@ -10,12 +10,41 @@ export default function ProjectSection() {
         {Projects.map((item) => {
           return (
             <div className={styles.projectContainer} key={item.logo}>
+              <div className={styles.projectLogo}>
+                <img src={item.logo} alt={item.title} />
+              </div>
+
               <p className={styles.projectTitle}>{item.title}</p>
-              <img
-                src={item.image}
-                alt={item.title}
-                className={styles.projectImage}
-              />
+
+              <div className={styles.imageWrapper}>
+                <div className={styles.projectLink}>
+                  <span className={styles.dot1}></span>
+                  <span className={styles.dot2}></span>
+                  <span className={styles.dot3}></span>
+
+                  <a href={item.path}>View Project</a>
+                </div>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className={styles.projectImage}
+                />
+              </div>
+
+              <h3 className={styles.subTitle}>{item.subTitle}</h3>
+              <p className={styles.projectDescription}>{item.description}</p>
+              <div className={styles.techstackWrapper}>
+                {item.techStack.map((tech, i) => {
+                  const Icon = tech.icon;
+                  return (
+                    <Icon
+                      className={styles.projectTechstack}
+                      key={i}
+                      style={{ color: tech.color }}
+                    />
+                  );
+                })}
+              </div>
             </div>
           );
         })}
--- client/config/projects.ts
+++ client/config/projects.ts
@@ -1,11 +1,22 @@
+import { FaBootstrap, FaCss3Alt } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiFastapi, SiPython } from "react-icons/si";
+
 export const Projects = [
   {
-    logo: "",
+    logo: "/project/logo/esignmo.png",
     title: "eSign Mo",
-    image: "/project/esignmo.png",
-    projectName: "eSign Mo",
+    image: "/project/projectImage/esignmo.png",
+    subTitle: "Sign Language Translation System",
+    path: "https://esignmo.vercel.app/main/asl",
     description:
-      "eSignMo: A Web-based System for Bridging Communication Barriers for Non-verbal Individual Through Sign Language Translation",
-    techStack: "",
+      "A web-based system that enables communication between non-verbal/deaf individuals and verbal users through sign-to-text and speech-to-sign translation. It also includes learning and testing modules for improving sign language skills.",
+    techStack: [
+      { icon: RiNextjsFill, name: "Next.js", color: "#000000" },
+      { icon: FaBootstrap, name: "Bootstrap", color: "#c013cf7c" },
+      { icon: FaCss3Alt, name: "CSS3", color: "#2965f1" },
+      { icon: SiFastapi, name: "FastAPI", color: "#009688" },
+      { icon: SiPython, name: "Python", color: "#3776AB" },
+    ],
   },
 ];
