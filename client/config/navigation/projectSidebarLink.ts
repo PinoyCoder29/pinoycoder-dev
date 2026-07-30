6aa69a3fef070181ababb9e369f90806833d3256
PinoyCoder29 <bicojayvee4@gmail.com>
2026-07-30 11:29:12 +0800

    updated navigation ts

--- client/config/navigation/projectSidebarLink.ts
+++ client/config/navigation/projectSidebarLink.ts
@@ -3,5 +3,5 @@ import { Projects } from "../projects";
 export const ProjectSidebarLinks = Projects.map((project) => ({
   name: `${project.hero?.title}`,
   path: `/projects/${project.slug}`,
-  icons: `${project.icons}`,
+  icons: `bi bi-folder-fill`,
 }));
